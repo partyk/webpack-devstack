aaa4a28c5f0850427da86f82ba00cb0f7493bcb2
Bohdan Partyk <info@podebrady.org>
2019-07-03 07:45:27 +0200

    add optimize image

--- webpack/loaders/webpack.image.js
+++ webpack/loaders/webpack.image.js
@@ -15,6 +15,30 @@ module.exports = ({include, exclude} = {}) => ({
                             outputPath: 'images/',
                             publicPath: config.path.publicPathAssets + 'images/'
                         }
+                    },
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: {
+                                progressive: true,
+                                quality: 65
+                            },
+                            // optipng.enabled: false will disable optipng
+                            optipng: {
+                                enabled: false
+                            },
+                            pngquant: {
+                                quality: '65-90',
+                                speed: 4
+                            },
+                            gifsicle: {
+                                interlaced: false
+                            },
+                            // the webp option will enable WEBP
+                            webp: {
+                                quality: 75
+                            }
+                        }
                     }
                 ]
             }
