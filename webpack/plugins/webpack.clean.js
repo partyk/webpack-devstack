f33bf48d9fec52ab9c8a4c7b593bb390c6c42962
Bohdan Partyk <info@podebrady.org>
2019-08-06 07:15:55 +0200

    update clean-webpack-plugin

--- webpack/plugins/webpack.clean.js
+++ webpack/plugins/webpack.clean.js
@@ -2,7 +2,7 @@
  * @link @link https://github.com/johnagan/clean-webpack-plugin
  * @type {CleanWebpackPlugin}
  */
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const deepmerge = require('deepmerge');
 const config = require('../../webpack.config');
 /**
@@ -10,10 +10,9 @@ const config = require('../../webpack.config');
  * @param options {array}
  * @returns {CleanWebpackPlugin}
  */
-module.exports = (options = []) => {
-    options = deepmerge([
-        config.path.assets + '*'
-    ],
-    options);
+module.exports = (options = {}) => {
+    options = deepmerge({
+        cleanOnceBeforeBuildPatterns: [config.path.assets + '**/*']
+    }, options);
     return new CleanWebpackPlugin(options);
 };
