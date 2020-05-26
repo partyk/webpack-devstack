a90ad2b16f5341480a538e48ec66956cef93f8e7
Bohdan Partyk <info@podebrady.org>
2020-05-26 18:32:30 +0200

    update package

--- webpack/plugins/webpack.copy.js
+++ webpack/plugins/webpack.copy.js
@@ -1,15 +1,15 @@
-/*
-@link doc https://github.com/webpack-contrib/copy-webpack-plugin
- */
+// @link doc https://github.com/webpack-contrib/copy-webpack-plugin
 const config = require('../../webpack.config');
 const deepmerge = require('deepmerge');
 const CopyPlugin = require('copy-webpack-plugin');
 
-module.exports = (options = []) => {
-    options = deepmerge([{
-        from: config.dir.src + '/images',
-        to: 'images'
-    }],
-    options);
+module.exports = (options = {}) => {
+    options = deepmerge({
+            patterns: [{
+                from: config.dir.src + '/images',
+                to: 'images'
+            }]
+        },
+        options);
     return new CopyPlugin(options);
 };
